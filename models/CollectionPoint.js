5b6b5332cadb94ddea3bd6f571705b3b30ba4730
Daniel <danielmartins888@gmail.com>
2024-09-16 13:00:22 -0300

    ajuste no Collection model

--- models/CollectionPoint.js
+++ models/CollectionPoint.js
@@ -14,6 +14,11 @@ const CollectionPointSchema = new mongoose.Schema({
         type: Number,
         required: true,
     },
+
+    email: {
+        type: String,
+        required: true,
+    },
    
     address: {
         type: String,
